c590fe8f630af1be4899b4b84a22edbe079ea887
r-moore98 <ryan.moore704@yahoo.com>
2023-04-12 21:59:02 +0000

    first version

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 /* eslint-disable */
+"use strict";
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// window.onload =function() {};
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let n = 0; n < noun.length; n++) {
+      // console.log("${pronoun[i]adj[j]}.com");
+      let div = document.createElement("div");
+      div.innerText = `${pronoun[i]}${adj[j]}${noun[n]}.com`;
+      document.body.appendChild(div);
+    }
+  }
+}
